10fc964f83c1e905242e190ccdbcdad0ac44f381
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-05 08:48:29 +0530

    WIP

--- src/modify.ts
+++ src/modify.ts
@@ -21,130 +21,124 @@ export default async function modify(
 
   return account
     ? await (async () => {
-        const accountCreationResult = await expr.firstAsync([
-          [
-            () => typeof args.enable !== "undefined",
-            async () => {
-              await authService.enable(externalUsername, pool, context);
-              return new Response(
-                `The user ${account.username} was disabled.`,
-                messageId
-              );
-            }
-          ],
-          [
-            () => typeof args.disable !== "undefined",
-            async () => {
-              await authService.disable(externalUsername, pool, context);
-              return new Response(
-                `The user ${account.username} was disabled.`,
-                messageId
-              );
-            }
-          ],
-          [
-            () => typeof args.destroy !== "undefined",
-            async () => {
-              const result = await authService.destroy(
-                externalUsername,
-                pool,
-                context
-              );
-
-              try {
+        const accountCreationExpressions = async () =>
+          await expr.firstAsync([
+            [
+              () => typeof args.enable !== "undefined",
+              async () => {
+                await authService.enable(externalUsername, pool, context);
                 return new Response(
-                  `The user ${account.username} was deleted.`,
-                  messageId
-                );
-              } catch (ex) {
-                const code = ex.message.split(/:|\(/)[0];
-                return new Response(
-                  code === "CANNOT_DELETE_ACTIVE_ACCOUNT"
-                    ? `As a security measure, the user needs to be disabled before deleting it. Say 'user disable'.`
-                    : `Unable to delete the user ${account.username}.`,
+                  `The user ${account.username} was disabled.`,
                   messageId
                 );
               }
-            }
-          ]
-        ]);
+            ],
+            [
+              () => typeof args.disable !== "undefined",
+              async () => {
+                await authService.disable(externalUsername, pool, context);
+                return new Response(
+                  `The user ${account.username} was disabled.`,
+                  messageId
+                );
+              }
+            ],
+            [
+              () => typeof args.destroy !== "undefined",
+              async () => {
+                const result = await authService.destroy(
+                  externalUsername,
+                  pool,
+                  context
+                );
+
+                try {
+                  return new Response(
+                    `The user ${account.username} was deleted.`,
+                    messageId
+                  );
+                } catch (ex) {
+                  const code = ex.message.split(/:|\(/)[0];
+                  return new Response(
+                    code === "CANNOT_DELETE_ACTIVE_ACCOUNT"
+                      ? `As a security measure, the user needs to be disabled before deleting it. Say 'user disable'.`
+                      : `Unable to delete the user ${account.username}.`,
+                    messageId
+                  );
+                }
+              }
+            ]
+          ]);
+
+        const accountModExpressions = async () => {
+          const results = await expr.collectAsync([
+            [
+              () => typeof args.about !== "undefined",
+              async () => {
+                await authService.editAbout(
+                  args.about,
+                  externalUsername,
+                  pool,
+                  context
+                );
+                return "about text";
+              }
+            ],
+            [
+              () => typeof args.domain !== "undefined",
+              async () => {
+                await authService.editDomain(
+                  args.domain,
+                  externalUsername,
+                  pool,
+                  context
+                );
+                return "domain";
+              }
+            ]
+          ]);
+          return results.length
+            ? new Response(`Updated ${results.join(", ")}.`, messageId)
+            : undefined;
+        };
+
+        const accountPermissionExpressions = async () =>
+          await expr.firstAsync([
+            [
+              () => typeof args.link !== "undefined",
+              async () => {
+                await authService.addPermissions(
+                  account.username,
+                  args.link,
+                  externalUsername,
+                  ["POST"],
+                  pool,
+                  context
+                );
+                return new Response(``, messageId);
+              }
+            ],
+            [
+              () => typeof args.unlink !== undefined,
+              async () => {
+                await authService.addPermissions(
+                  account.username,
+                  args.unlink,
+                  externalUsername,
+                  ["POST"],
+                  pool,
+                  context
+                );
+                return new Response(``, messageId);
+              }
+            ]
+          ]);
 
         return (
-          accountCreationResult ||
-          (await (async () => {
-            const accountModResults = await expr.collectAsync([
-              [
-                () => typeof args.about !== "undefined",
-                async () => {
-                  await authService.editAbout(
-                    args.about,
-                    externalUsername,
-                    pool,
-                    context
-                  );
-                  return "about text";
-                }
-              ],
-              [
-                () => typeof args.domain !== "undefined",
-                async () => {
-                  await authService.editDomain(
-                    args.domain,
-                    externalUsername,
-                    pool,
-                    context
-                  );
-                  return "domain";
-                }
-              ]
-            ]);
-
-            return accountModResults.length
-              ? new Response(
-                  `Updated ${accountModResults.join(", ")}.`,
-                  messageId
-                )
-              : await (async () => {
-                  const changePermissionsResult = await expr.firstAsync([
-                    [
-                      () => typeof args.link !== "undefined",
-                      async () => {
-                        await authService.addPermissions(
-                          account.username,
-                          args.link,
-                          externalUsername,
-                          ["POST"],
-                          pool,
-                          context
-                        );
-                        return new Response(``, messageId);
-                      }
-                    ],
-                    [
-                      () => typeof args.unlink !== undefined,
-                      async () => {
-                        await authService.addPermissions(
-                          account.username,
-                          args.unlink,
-                          externalUsername,
-                          ["POST"],
-                          pool,
-                          context
-                        );
-                        return new Response(``, messageId);
-                      }
-                    ]
-                  ]);
-
-                  return (
-                    changePermissionsResult ||
-                    new Response(
-                      `Sorry, did not follow that instruction.`,
-                      messageId
-                    )
-                  );
-                })();
-          })())
+          (await accountCreationExpressions()) ||
+          (await accountModExpressions()) ||
+          (await accountPermissionExpressions()) ||
+          new Response(`Sorry, did not follow that instruction.`, messageId)
         );
       })()
     : new Response(
